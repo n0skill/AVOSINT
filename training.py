2f5c4c5d02e802cfec9fcee798234d4acb89eb35
n0skill <noskill@fixme.ch>
2017-08-10 22:12:54 +0200

    Fix update of plane path

--- training.py
+++ training.py
@@ -50,6 +50,7 @@ def main():
                             print(plane_obj[2])
                             point_to_add_to_path = ((latitude, longitude))
                             new_path = (plane_obj[2],) + point_to_add_to_path
+                            print(new_path)
                             curs.execute('UPDATE planes SET path = %s  WHERE number = %s', (new_path, numb))
 
                         # Else it is not yet in db. Add to db if we have number and position
