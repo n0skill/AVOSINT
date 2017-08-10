f86b116e2b85f164c8212e472b058c777c70466d
n0skill <noskill@fixme.ch>
2017-08-10 23:10:40 +0200

    Trying again and again

--- training.py
+++ training.py
@@ -60,6 +60,7 @@ def main():
 
                         # Else it is not yet in db. Add to db if we have number and position
                     if not flg:
+                        print(numb)
                         plane = Plane(webi, numb, callsign, latitude, longitude)
                         path =  (plane.coordinates.latitude, plane.coordinates.longitude)
                         curs.execute('INSERT INTO planes(number, callsign, path) values (%s, %s, \'[ %s ]\')', (plane.numb, plane.call, path))
