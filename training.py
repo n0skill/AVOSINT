fb337f78a90fbfdb053c4ee3da0d13af3131b923
n0skill <noskill@fixme.ch>
2017-08-11 14:19:45 +0200

    Try to make adapters

--- training.py
+++ training.py
@@ -98,7 +98,7 @@ def adapt_path(path):
 
 # should return an array
 def cast_path(str, curs):
-    m = re.match(r"\(\(([^)]+),([^)]+)\)\)", str)
+    m = re.match(r"\((([^)]+),([^)]+))\)", str)
     print(m)
     if m:
         return Coordinates(m.group(0), m.group(1))
