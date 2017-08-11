5ca16b4ef6770e485d7f8f37caac1d9110679627
n0skill <noskill@fixme.ch>
2017-08-11 13:29:22 +0200

    Try to make adapters

--- training.py
+++ training.py
@@ -56,7 +56,7 @@ def main():
                         path = curs.fetchone()
                         print(path)
                         point_to_add_to_path = Coordinates(latitude, longitude)
-                        path.append(point_to_add_to_path)
+                        path = path + point_to_add_to_path
                         curs.execute('UPDATE planes SET path = %s  WHERE number =  %s ', (path, numb))
 
                         # Else it is not yet in db. Add to db if we have number and position
