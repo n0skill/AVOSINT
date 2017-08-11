42f20936a4eb09ed7d56a81f7598a89f735b94ec
n0skill <noskill@fixme.ch>
2017-08-11 13:27:03 +0200

    Try to make adapters

--- training.py
+++ training.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 import os
 import time
 import psycopg2
+from psycopg2.extensions import adapt, register_adapter, AsIs
 
 PSQL_DB = "planes"
 PSQL_TABLE = "planes"
@@ -22,7 +23,8 @@ def main():
 
     conn = psycopg2.connect(host='localhost', dbname=PSQL_DB, user=args.psql_user, password=args.psql_pass)
     curs = conn.cursor()
-
+    register_adapter(Coordinates, adapt_point)
+    register_adapter(Coordinates[], adapt_path)
     # For each file of the day
     for filename in list_of_files:
         os.system('clear')
@@ -36,6 +38,7 @@ def main():
                 aviatos_list_from_file = j['acList']
                 leng = len(aviatos_list_from_file)
                 for i, aviato in enumerate(aviatos_list_from_file):
+                    path = []
                     os.sys.stdout.write('\r' + str(i+1) + '/' + str(leng))
                     webi = aviato.get('Reg')
                     numb = aviato.get('Reg')
@@ -51,19 +54,18 @@ def main():
                         flg = True
                         curs.execute('SELECT path from planes WHERE number =  %s ', (numb,))
                         path = curs.fetchone()
-                        print(path)
                         #print('Plane already in list ! Append position to the path')
-                        path_array = eval(path)
-                        point_to_add_to_path = (latitude, longitude)
-                        path_array.append(point_to_add_to_path)
-                        #print('New path: ' + str(path_array))
-                        curs.execute('UPDATE planes SET path = %s  WHERE number =  %s ', (str(path_array), numb))
+                        point_to_add_to_path = Coordinates(latitude, longitude)
+                        path.append(point_to_add_to_path)
+                        curs.execute('UPDATE planes SET path = %s  WHERE number =  %s ', (path, numb))
 
                         # Else it is not yet in db. Add to db if we have number and position
                     if not flg and numb is not None and latitude is not None:
                         plane = Plane(webi, numb, callsign, latitude, longitude)
-                        path = (plane.coordinates.latitude, plane.coordinates.longitude)
-                        curs.execute('INSERT INTO planes(number, callsign, path) values (%s, %s, %s)', (plane.numb, plane.call, str(path)))
+                        point = Coordinates(latitude, longitude)
+                        path = []
+                        path.append(point)
+                        curs.execute('INSERT INTO planes(number, callsign, path) values (%s, %s, %s)', (plane.numb, plane.call, path))
                         conn.commit()
                 print("")
                 time.sleep(0.1)
@@ -81,6 +83,17 @@ def printpath_and_classify(array):
     plt.plot(array)
     plt.show()
 
+def adapt_point(point):
+    x = adapt(point.latitude).getquoted()
+    y = adapt(point.longitude).getquoted()
+    return AsIs("'(%s, %s)'" % (x, y))
+
+def adapt_path(path):
+    pts = []
+    for point in path:
+        pt_ad = adapt_point(point)
+        pts.append(pt_ad)
+    return AsIs("'[%s]'" % pts)
 
 
 if __name__ == "__main__":
