2b768eec6b024eb387b242652fff5e26d496f65e
n0skill <noskill@fixme.ch>
2017-08-11 16:24:22 +0200

    Fix path insert query syntax

--- training.py
+++ training.py
@@ -56,7 +56,7 @@ def main():
                         path_index = path_obj[1]
                         print('Path index is ' + str(path_index))
                         path_index = path_index + 1
-                        curs.execute('INSERT INTO path (number, index, point_x, point_y) values (%s, %s) WHERE number =  %s', (numb, path_index, latitude, longitude))
+                        curs.execute('INSERT INTO path (number, index, point_x, point_y) values (%s, %s, %s, %s)', (numb, path_index, latitude, longitude))
                         conn.commit()
 
                         # Else it is not yet in db. Add to db if we have number and position
