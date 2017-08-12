31f1e1b429c8af084fb6cde673550460d3fb2054
n0skill <noskill@fixme.ch>
2017-08-12 11:05:08 +0200

    move sleep time to place where it belongs

--- training.py
+++ training.py
@@ -78,7 +78,7 @@ def main():
                             curs.execute('INSERT INTO path (number, index, point_x, point_y) values (%s, %s, %s, %s)', (numb, 0, latitude, longitude))
                             conn.commit()
                             print("")
-                        time.sleep(0.2)
+                        time.sleep(0.05)
 
                 except ValueError as e:
                     pass
