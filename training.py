5cf0547a9f03ac26e941560bb25394f072046367
n0skill <noskill@fixme.ch>
2017-08-13 12:18:19 +0200

    Actually insert if there is no previous path with plane reg number in path table

--- training.py
+++ training.py
@@ -64,12 +64,16 @@ def main():
                             flg = True
                             curs.execute('SELECT * from path where number = %s ORDER BY index DESC LIMIT 1', (numb,)) # order by index
                             rep = curs.fetchone()
-                            if rep is not None:
+
+                            # No path yet
+                            if rep is None:
+                                curs.execute('INSERT INTO path (number, index, point_x, point_y) values (%s, %s, %s, %s)', (numb, 0, latitude, longitude))
+                            else:
                                 path_index = rep[1]
                             #print('Path index is ' + str(path_index))
                                 path_index = path_index + 1
                                 curs.execute('INSERT INTO path (number, index, point_x, point_y) values (%s, %s, %s, %s)', (numb, path_index, latitude, longitude))
-                                conn.commit()
+                            conn.commit()
 
                         # Else it is not yet in db. Add to db if we have number and position
                         elif not flg and numb is not None and latitude is not None:
