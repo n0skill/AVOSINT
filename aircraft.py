0730f74e8f2cc548d6fb4c592256080fceb51a0e
n0skill <marc.savioz@gmail.com>
2026-01-29 09:42:51 +0100

    Refactor Aircraft class a bit

--- aircraft.py
+++ aircraft.py
@@ -6,24 +6,24 @@ from geoloc import Coordinates
 
 # Decoders for countries here
 
+
 class Aircraft:
-    def __init__(self, tail_n, msn=None, call=None, \
-            latitude=None, longitude=None, craft_type=None, \
-            origin=None, destination=None, altitude=None, \
-            manufacturer=None, icao=None,notes=None):
+    def __init__(self, tail_n, msn=None, call=None,
+                 latitude=None, longitude=None, craft_type=None,
+                 origin=None, destination=None, altitude=None,
+                 manufacturer=None, icao=None, notes=None):
 
-        self.tail_n         = tail_n
-        self.msn            = msn
-        self.call           = call
-        self.origin         = origin
-        self.manufacturer   = manufacturer
-        self.destination    = destination
-        self.altitude       = altitude	
-        self.latitude       = latitude
-        self.longitude      = longitude
-        self.icao           = icao  
-        self.notes          = notes
-
+        self.tail_n = tail_n
+        self.msn = msn
+        self.call = call
+        self.origin = origin
+        self.manufacturer = manufacturer
+        self.destination = destination
+        self.altitude = altitude
+        self.latitude = latitude
+        self.longitude = longitude
+        self.icao = icao
+        self.notes = notes
 
     def __str__(self):
         return self.__repr__()
@@ -37,10 +37,9 @@ class Aircraft:
         Call Sign: %s
         Last known position: %s
         Last known altitude: %s
-        
+
         Notes: 
         %s
-        """ % (self.manufacturer, self.msn, 
-                self.icao, self.tail_n,
-                self.call, (self.latitude, self.longitude), 
-                self.altitude, self.notes)
+        """ % (self.manufacturer, self.msn, self.icao, self.tail_n,
+               self.call, (self.latitude, self.longitude), self.altitude,
+               self.notes)
